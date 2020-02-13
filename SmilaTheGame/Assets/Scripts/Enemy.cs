2e7cb524af92fffc7178f98bd2e0c18fd167c5eb
Tapani Alastalo <tapani.alastalo@gmail.com>
2020-02-13 22:13:16 +0200

    basic tests

---
+++ SmilaTheGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 2f;
+
+    public static int EnemiesAlive = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemiesAlive++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > health)
+        {
+            //Die();
+        }
+        Debug.Log(collision.relativeVelocity.magnitude);
+    }
+
+    /*
+    private void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0)
+        {
+            //SceneManager.
+            Debug.Log("Level Won");
+        }
+        Destroy(gameObject);
+    }
+    */
+}
